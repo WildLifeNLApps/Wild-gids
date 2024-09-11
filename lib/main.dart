a494c337f58fa03be8ee7b1339be7f0399477fc1
MarkvdKort <markvanderkort@gmail.com>
2024-09-11 10:33:42 +0200

    feat: setup flavors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:wildgids/app.dart';
+import 'package:wildgids/config/app_config.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  PackageInfo info = await PackageInfo.fromPlatform();
+  AppConfig.create(flavor: getFlavorByPackageName(info.packageName));
   runApp(const App());
 }
