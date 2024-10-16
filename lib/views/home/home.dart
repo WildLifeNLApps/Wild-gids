4abb650cae36b4ef823bba193dc57cd538290462
RensVanGiersbergen <58734636+RensVanGiersbergen@users.noreply.github.com>
2024-10-16 12:55:31 +0200

    Added basic PoC map (#24)
    * poc: added map view in home view
    
    * feat: added map boundary, zoom and marker
    
    * fix: map marker now shows

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:wildgids/config/theme/asset_icons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    const startingPoint = LatLng(51.25851739912562, 5.622422796819703);
+
+    return FlutterMap(
+      mapController: MapController(),
+      options: MapOptions(
+          initialCenter: startingPoint, // Center the map over Weerterbos
+          initialZoom: 11,
+
+          // Set map zoom and location boundaries
+          minZoom: 9,
+          maxZoom: 18,
+          cameraConstraint: CameraConstraint.contain(
+              bounds: LatLngBounds(const LatLng(52.25851, 6.6224),
+                  const LatLng(50.25851, 4.6224)))),
       children: [
-        Text(
-          "Home",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+        TileLayer(
+          // Display map tiles from any source
+          urlTemplate:
+              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+          userAgentPackageName: 'com.wildlifenl.wildgids',
         ),
+        MarkerLayer(markers: [
+          Marker(
+              point: startingPoint,
+              width: 30,
+              height: 30,
+              child: SvgPicture.asset(AssetIcons.locationDot),
+              rotate: true),
+        ])
       ],
     );
   }
